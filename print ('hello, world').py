46a8be8c5372d8bfd3befa0ec477fa15534d60f6
exactxacto <64893872+exactxacto@users.noreply.github.com>
2025-10-28 13:34:44 +0000

    deleted unnecessary files

--- print ('hello, world').py
+++
@@ -1,9 +0,0 @@
-print ('hello, world')
-# This code prints "hello, world" to the console.
-def number_squared():
-    for i in range(5): 
-        x = i*i
-        print (x)
-#ive now made a change
-number_squared()
-
