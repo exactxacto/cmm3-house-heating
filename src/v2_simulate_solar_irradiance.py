705d3afc113cfedbdc5956dd939364c5e2e07c70
exactxacto <64893872+exactxacto@users.noreply.github.com>
2025-10-29 12:56:25 +0000

    reads SolRad.xslx and takes in input as q_val

--- src/v2_simulate_solar_irradiance.py
+++ src/v2_simulate_solar_irradiance.py
@@ -13,10 +13,11 @@ except OSError:
 
 # Load solar irradiance data from file
 try:
-    q_val = np.loadtxt('solar_irradiance/out/solar_irradiance_data.txt', usecols=(1,), skiprows=1)
+    df_q_val = pd.read_excel('SolRad.xlsx', skiprows=2, usecols=[4], header=None)
+    q_val = df_q_val.iloc[:, 0].values
 except OSError:
     print ("File not found. Please ensure the solar irradiance data file exists at the specified path.")
-    r_val = np.array([])  # Assign an empty array if file not found
+    df_q_val = pd.DataFrame()  # Assign an empty DataFrame if file not found
 
 # Parameters
 width_house = 9.0  # meters
@@ -56,4 +57,6 @@ def calculate_T_out_eff(t):  #Returns a 2D array [n_hours x n_materials] of effe
 T_out_eff = calculate_T_out_eff(r_val, q_val, T_amb_hourly)
 time_index = pd.RangeIndex(start=0, stop=n_hours, step=1, name="hour")
 T_out_df = pd.DataFrame(T_out_eff, index=time_index, columns=[f"R={R:.2f}" for R in r_val])
+
+
 
