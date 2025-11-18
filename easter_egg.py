875ed6b0f558f2f2dac58ffa7fa72f5f90347c8e
christina-ft <cfotou4@gmail.com>
2025-11-18 15:08:12 +0000

    Easter egg added

---
+++ easter_egg.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 18 15:00:36 2025
+
+@author: Christina
+"""
+
+import webbrowser
+
+# opens YouTube link in browser 
+webbrowser.open("https://www.youtube.com/watch?v=nYMeJSehCe4")
+
